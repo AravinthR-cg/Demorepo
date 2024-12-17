a8fcb6fc5a561786a7490a42f424113dd32f6580
R <aravinth.d.r@capgemini.com>
2024-12-17 16:33:04 +0530

    Second commit

--- src/GITPackage/Simple.java
+++ src/GITPackage/Simple.java
@@ -5,6 +5,7 @@ public class Simple {
 	public static void main(String[] args) {
 		System.out.println("Hi from STS");
 		System.out.println("Hi from GITHUB");
+		System.out.println("Welcome again from Sts");
 	}
 
 }
